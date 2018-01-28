af49ee20cd391e389050c7b737d96e5fc459c3b0
njang <neo.w.jang@gmail.com>
2018-01-27 22:26:37 -0600

    Refactor ticker function

--- public/javascripts/ticker.js
+++ public/javascripts/ticker.js
@@ -1,52 +1,20 @@
 let ds = 0;
-let max = 10;
 
-const circleEmpty = () => {
-	$('#decimalSeconds').append($('<ticker>', {html: '&#9898;&nbsp;'}));
-}
-
-const circleFill = () => {
-	$('#decimalSeconds').append($('<ticker>', {html: '&#9899;&nbsp;'}));
-}
+// Define two types of tickers: empty circle and filled circle
+const circleEmpty = () => {	$('#decimalSeconds').append($('<ticker>', {html: '&#9898;&nbsp;'})) }
+const circleFill =  () => {	$('#decimalSeconds').append($('<ticker>', {html: '&#9899;&nbsp;'})) }
 
 const ticker = () => {
-  let index = ds%max + 1;
+  let index = ds % 10 + 1;
   $('#decimalSeconds').html('&nbsp;');
-  switch (index) {
-  	case 1:
-  		circleEmpty();circleFill();circleFill();circleFill();circleFill();
-  		break;
-  	case 2:
-  		circleEmpty();circleEmpty();circleFill();circleFill();circleFill();
-  		break;
-  	case 3:
-  		circleEmpty();circleEmpty();circleEmpty();circleFill();circleFill();
-  		break;
-  	case 4:
-  		circleEmpty();circleEmpty();circleEmpty();circleEmpty();circleFill();
-  		break;
-  	case 5:
-  		circleEmpty();circleEmpty();circleEmpty();circleEmpty();circleEmpty();
-  		break;
-  	case 6:
-  		circleFill();circleEmpty();circleEmpty();circleEmpty();circleEmpty();
-  		break;
-  	case 7:
-  		circleFill();circleFill();circleEmpty();circleEmpty();circleEmpty();
-  		break;
-  	case 8:
-  		circleFill();circleFill();circleFill();circleEmpty();circleEmpty();
-  		break;
-  	case 9:
-  		circleFill();circleFill();circleFill();circleFill();circleEmpty();
-  		break;
-  	case 10:
-  		circleFill();circleFill();circleFill();circleFill();circleFill();
-  		break;  		
+  // construct sequence of 5 circles
+  for (let i = 0; i < 5; i++) {
+  	((i < index && index < 5) || !((i >= index && index < 5) || (i < index - 5 && index >= 5))) ? circleEmpty() :circleFill();
   }
 
   // count up to 10 and reset
   ds < 9 ? ds++ : ds = 0
 }
-setInterval(ticker, 846);
 
+// Update every beat, which equals 846 milliseconds
+setInterval(ticker, 846);
