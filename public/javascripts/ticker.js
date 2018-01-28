dd0ed9244b59bac2b003a4a6ca21851d37cd5197
njang <neo.w.jang@gmail.com>
2018-01-27 22:03:15 -0600

    Complete ticker feature

--- public/javascripts/ticker.js
+++ public/javascripts/ticker.js
@@ -1,26 +1,52 @@
-let d = 0;
-let max = 5;
-const circleEmpty = $('<span>', {text: '&#9898;'});
-const circleFill = $('<span>', {text: '&#9899;'});
+let ds = 0;
+let max = 10;
+
+const circleEmpty = () => {
+	$('#decimalSeconds').append($('<ticker>', {html: '&#9898;&nbsp;'}));
+}
+
+const circleFill = () => {
+	$('#decimalSeconds').append($('<ticker>', {html: '&#9899;&nbsp;'}));
+}
 
 const ticker = () => {
-  // let d = new Date();
-  // $('#demo').text(d.toLocaleTimeString());
-  
-  $('#decimalSeconds').text(d);
-  let index = d%max+1;
-  
-  $('#decimalSeconds').text(index);
-
-  for (let i = 0; i < max; i++) {
-  	// $('#decimalSeconds').clear();
-  	if (i < d) {
-			$('#decimalSeconds').append(circleFill);
-		} else {
-			$('#decimalSeconds').append(circleEmpty);
-		}
+  let index = ds%max + 1;
+  $('#decimalSeconds').html('&nbsp;');
+  switch (index) {
+  	case 1:
+  		circleEmpty();circleFill();circleFill();circleFill();circleFill();
+  		break;
+  	case 2:
+  		circleEmpty();circleEmpty();circleFill();circleFill();circleFill();
+  		break;
+  	case 3:
+  		circleEmpty();circleEmpty();circleEmpty();circleFill();circleFill();
+  		break;
+  	case 4:
+  		circleEmpty();circleEmpty();circleEmpty();circleEmpty();circleFill();
+  		break;
+  	case 5:
+  		circleEmpty();circleEmpty();circleEmpty();circleEmpty();circleEmpty();
+  		break;
+  	case 6:
+  		circleFill();circleEmpty();circleEmpty();circleEmpty();circleEmpty();
+  		break;
+  	case 7:
+  		circleFill();circleFill();circleEmpty();circleEmpty();circleEmpty();
+  		break;
+  	case 8:
+  		circleFill();circleFill();circleFill();circleEmpty();circleEmpty();
+  		break;
+  	case 9:
+  		circleFill();circleFill();circleFill();circleFill();circleEmpty();
+  		break;
+  	case 10:
+  		circleFill();circleFill();circleFill();circleFill();circleFill();
+  		break;  		
   }
-  d++;
-  debugger;
+
+  // count up to 10 and reset
+  ds < 9 ? ds++ : ds = 0
 }
-setInterval(ticker ,846);
+setInterval(ticker, 846);
+
